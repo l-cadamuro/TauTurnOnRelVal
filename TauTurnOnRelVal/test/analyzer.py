5073a7e8e7b80edbfd611edbee2847d02d0bead6
Luca Cadamuro <lc.cadamuro@gmail.com>
2017-04-10 16:52:36 +0200

    input files

--- TauTurnOnRelVal/test/analyzer.py
+++ TauTurnOnRelVal/test/analyzer.py
@@ -3,7 +3,7 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("RelValTauL1")
 
-is80X = True
+is80X = False
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
@@ -11,9 +11,6 @@ if is80X:
   process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_v20'
   process.source = cms.Source("PoolSource",
       fileNames = cms.untracked.vstring(
-          # '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/90X_upgrade2017_realistic_v20-v1/00000/580B472C-CC0F-E711-8393-0CC47A7C357E.root',
-          # '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/90X_upgrade2017_realistic_v20-v1/00000/2C3ED96A-CC0F-E711-B53D-0CC47A7C360E.root',
-          # '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/90X_upgrade2017_realistic_v20-v1/00000/344E9376-CB0F-E711-8AF8-0025905A48E4.root',
         '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/FEBC4F3B-8E98-E611-881F-0CC47A4D766C.root',
         '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/4CB37ED1-8E98-E611-B36C-0CC47A7452DA.root',
         '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/3C932A4B-8F98-E611-814E-0CC47A4D76B6.root',
@@ -29,17 +26,14 @@ else:
   process.GlobalTag.globaltag = '90X_mcRun2_asymptotic_v5'
   process.source = cms.Source("PoolSource",
       fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/90X_upgrade2017_realistic_v20-v1/00000/580B472C-CC0F-E711-8393-0CC47A7C357E.root',
-        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/90X_upgrade2017_realistic_v20-v1/00000/2C3ED96A-CC0F-E711-B53D-0CC47A7C360E.root',
-        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/90X_upgrade2017_realistic_v20-v1/00000/344E9376-CB0F-E711-8AF8-0025905A48E4.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/FEBC4F3B-8E98-E611-881F-0CC47A4D766C.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/4CB37ED1-8E98-E611-B36C-0CC47A7452DA.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/3C932A4B-8F98-E611-814E-0CC47A4D76B6.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/24D05AE6-8F98-E611-9F9A-0025905A60D2.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/400FA080-9198-E611-B675-0CC47A7AB7A0.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/F8FCC5DC-9198-E611-B84A-0CC47A4D768E.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/CC7D9CED-A098-E611-8F1B-0025905A608E.root',
-        # '/store/relval/CMSSW_8_0_21/RelValZTT_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v6_Tr4GT_v6-v1/10000/1E8C2AEB-A098-E611-A0CC-0CC47A4C8F08.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/EE445371-7810-E711-86D9-0CC47A7C345C.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/8EEA2072-7810-E711-ABF6-0025905A610A.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/F425E50A-1110-E711-B821-0CC47A4D7628.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/0C073C1F-1110-E711-B79A-0CC47A4C8E28.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/663779D8-1210-E711-9006-0CC47A78A4B8.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/D035C60F-1210-E711-867E-0CC47A78A2EC.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/AC5D5211-1210-E711-80FA-0CC47A4D7628.root',
+        '/store/relval/CMSSW_9_0_0/RelValZTT_13/GEN-SIM-RECO/PU25ns_90X_mcRun2_asymptotic_v5-v1/00000/0099F6EB-1210-E711-A364-0CC47A78A458.root',
       )
   )
 
